753d02c6ae05b646678f175e5f5cb4672b702dbc
Suryansh3105 <suryanshpandeywork@gmail.com>
2026-01-19 08:19:50 +0530

    refactoring task server project

--- http/task server/index.js
+++ http/task server/index.js
@@ -1,5 +1,10 @@
 const http =require("node:http");
+// got logging we will use fs module 
+const fs=require('node:fs');
 
+
+
+// my attempt
 const tweets= [
     {
         name:"surya",
@@ -11,35 +16,79 @@ const tweets= [
     }
 ]
 
+// const server=http.createServer(function (req,res){
+//     // handling request
+//    console.log(req.method); console.log(req.url);
+//     // handing respone
+//         if(req.url=='/'&& req.method=='GET'){
+//             res.writeHead(200);
+//             res.end("Hello welcome to my server");
+            
+//         }
+//         else if(req.url=='/contact-us'&& req.method=='GET'){
+//             res.writeHead(200);
+//             res.end("email: email.com phone number : 123456789");
+            
+//         }
+//         else if(req.url=='/tweet'&& req.method=='POST'){
+//             res.writeHead(201);
+//             res.end("DB operation done");
+            
+//         }
+//          else if(req.url=='/tweet'&& req.method=='GET'){
+//             res.writeHead(200);
+//             res.end(tweets); // this give the erro due res.end can only take strings,buffer , unit8array you are trying to send js array of object solution convert it string through JSON.stringify(variableName);
+            
+//         }
+//         else{
+//             res.writeHead(404);
+//             res.end("wrong page requested");
+//         }
+        
+//     }
+// );
+
+// improving through learning 
+
 const server=http.createServer(function (req,res){
     // handling request
-    console.log(req.method);
-    console.log(req.url);
+    const method=req.method;
+    const path=req.url;
+    const log=`[${Date.now()} : ${method} , ${path}] \n`;
+    fs.appendFile('log.txt',log,'utf-8',(err)=>{
+        console.log(err);
+    });
     // handing respone
-        if(req.url=='/'&& req.method=='GET'){
-            res.writeHead(200);
-            res.end("Hello welcome to my server");
-            
+    switch(method){
+        case 'GET':{
+            switch(path){
+                case '/':
+                    res.writeHead(200).end("Hello welcome to my server"); break;
+                case '/contact-us':
+                     res.writeHead(200).end("email: email.com phone number : 123456789"); break;
+                case '/tweet': res.writeHead(200).end(JSON.stringify(tweets)); break;
+                default:{res.writeHead(404).end("wrong path request")
+
         }
-        else if(req.url=='/contact-us'&& req.method=='GET'){
-            res.writeHead(200);
-            res.end("email: email.com phone number : 123456789");
-            
+            }
+
+        }break;
+        case 'POST':{
+            switch(path){
+                case '/tweet':
+                    res.writeHead(201).end("DB operation done"); break;
+                default:{res.writeHead(404).end("wrong path request")
+
         }
-        else if(req.url=='/tweet'&& req.method=='POST'){
-            res.writeHead(201);
-            res.end("DB operation done");
-            
+            }
+
+        }break;
+        default:{
+            res.writeHead(404).end("wrong path request");
+
         }
-         else if(req.url=='/tweet'&& req.method=='GET'){
-            res.writeHead(200);
-            res.end(tweets);
-            
-        }
-        else{
-            res.writeHead(404);
-            res.end("wrong page requested");
-        }
+    }
+        
         
     }
 );
